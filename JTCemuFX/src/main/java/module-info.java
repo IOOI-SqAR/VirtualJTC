f4b051fe432fc95ac20844bb321835f4f185782a
Lars Sonchocky-Helldorf <lars.sonchocky-helldorf@hamburg.de>
2024-01-02 02:18:09 +0100

    getting closer …

--- JTCemuFX/src/main/java/module-info.java
+++ JTCemuFX/src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module org.jens_mueller.jtcemu.platform.fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.jens_mueller.jtcemu.platform.fx to javafx.fxml;
+    opens org.jens_mueller.jtcemu.platform.fx;
     exports org.jens_mueller.jtcemu.platform.fx;
 }
